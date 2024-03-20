11e2c72abdf4388366f8c13cc759a17153066e53
PREETHI-B0 <bpreethi663@gmail.com>
2024-03-20 20:02:03 +0530

    commited sucessfully

---
+++ Project/weather/weather-react/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WeatherComponent from './WeatherComponent/WeatherComponent'
+const App = ( ) => {
+    return (
+        <React.Fragment>
+            <WeatherComponent/>
+        </React.Fragment>
+    )
+}
+export default App;
